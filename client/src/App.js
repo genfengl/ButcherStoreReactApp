1557bc66b956ba7c14b535a17d61af8d08c9e750
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-10-28 16:22:45 +1100

    FE component update and route test

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { application } from 'express';
+import { Routes, Route, NavLink, Link, Outlet, useParams } from 'react-router-dom'
 import './App.css';
 
 const Header = () => {
@@ -11,12 +13,44 @@ const Header = () => {
 const NavBar = () => {
   return (
     <div className="navBar">
-      <ul>
+      {/* <ul>
         <li>Selection of Meats</li>
         <li>About Us</li>
         <li>Contact</li>
-        
-      </ul>
+      </ul> */}
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </div>
+  )
+}
+const LoginForm = (props) => {
+  const handleSubmit = (event) => {
+  //  res.redirect('/login') ???
+  }
+  return (
+    <form>
+      <label>Username:</label>
+      <input type='text'>Username</input>
+      <br />
+      <label>Password:</label>
+      <input type='password'>Password</input>
+      <br />
+      <input type='submit' onSubmit={handleSubmit}>Login</input>
+    </form>
+  )
+}
+
+const LoginPage = (props, LoginForm) => {
+  return (
+    <div>
+    <h1>Login</h1>
+    <img src={props.imageURL} alt="company logo" />
+    <LoginForm />
     </div>
   )
 }
