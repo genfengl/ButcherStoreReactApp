1f46ee3a34b631bdc5b4539bb1a45035acf44e19
jdyball11 <jack.dyball92@gmail.com>
2022-10-28 13:58:52 +1100

    pushed data to mongodb cloud

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+
+const Meat = require('./models/meat')
 
 const app = express()
-const PORT = 3999
+const PORT = process.env.PORT
+const dbURL = process.env.MONGODB_URL
+// const MongoDBStore = mongoDBSession(session)
+// const sessionStore = new MongoDBStore({
+//   uri: dbURL,
+//   collection: 'sessions'
+// })
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+mongoose.connect(dbURL, (err) => {
+  console.log('Connected to db')
+  console.log(err)
+})
 
 app.listen(PORT, () => {
     console.log('Connected to veneer server on PORT', PORT)
   })
 
 
-//route
-
-
-// merge conflict testing
-
-app.get('/home', (req, res) => {
-    res.send(`Eat my ass`)
-})
 
