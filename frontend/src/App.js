535ac4342fc18d50807d001490d6642ea8824aca
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-10-28 11:53:35 +1100

    initial components in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+    <h4 className="header-title">Header</h4>
+    </div>
+  )
+}
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <ul>
+        <li>Selection of Meats</li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <NavBar />
     </div>
   );
 }
