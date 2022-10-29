eff8ec2622afc77799ce2933027d5add92ee7f13
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-10-29 22:05:08 +1100

    basic login forms and pages

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import LoginForm from "./LoginForm"
+
+const LoginPage = () => {
+    return (
+      <div>
+      <h1>Login</h1>
+      <img alt="company logo" />
+      <LoginForm />
+      <p>Not registered? <a href="#">Register here</a></p>
+      </div>
+    )
+  }
+  export default LoginPage
---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,18 @@
+
+const RegisterForm = () => {
+    return (
+      <form method='put'>
+        <label>Username:</label>
+        <input type='text' placeholder='Username' />
+        <br />
+        <label>Password:</label>
+        <input type='password' placeholder='Password'/>
+        <br />
+        <input type='submit' value='Register' />
+      </form>
+    )
+  }
+
+  //need to do error flash's for duplicate username 100% and then try { add to schema} catch { err }
+
+  export default RegisterForm
