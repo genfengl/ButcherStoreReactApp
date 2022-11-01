e55b8e5621a25fc80e2dcaeb5bebad76985b9008
jdyball11 <jack.dyball92@gmail.com>
2022-11-01 23:18:54 +1100

    add to cart

--- client/src/CartContext.js
+++ client/src/CartContext.js
@@ -1,23 +1,98 @@
 import { createContext } from "react";
-
+import { useState } from "react";
 
 export const CartContext = createContext({
+    items: [],
     getProductQuantity: () => {},
     addOneToCart: () => {},
     removeOneFromCart: () => {},
-    deleteFromCart: () => {}
+    deleteFromCart: () => {},
+    getTotalCost: () => {}
 })
 
 
 export function CartProvider({children}) {
     const [cartProducts, setCartProducts] = useState([])
+    
+    const getProductQuantity = (id) => {
+        const quantity = cartProducts.find(product => product.id === id)?.quantity
+
+        if (quantity === undefined) {
+            return 0
+        }
+        return quantity
+    }
+
+
+    const addOneToCart = (id) => {
+        const quantity = getProductQuantity(id)
+
+        if (quantity === 0) {
+            setCartProducts(
+                [
+                    ...cartProducts,
+                    {
+                        id: id, 
+                        quantity: 1
+                    }
+                ]
+            )
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id
+                    ? { ...product, quantity: product.quantity + 1 }
+                    : product
+                )
+            )
+        }
+    }
+
+    const removeOneFromCart = (id) => {
+        const quantity = getProductQuantity(id)
+
+        if (quantity == 1) {
+            deleteFromCart(id)
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id
+                    ? { ...product, quantity: product.quantity - 1 }
+                    : product
+                )
+            )
+        }
+    }
+
+
+    const deleteFromCart = (id) => {
+        setCartProducts(cartProducts => 
+                cartProducts.filter(currentProduct => {
+                    return currentProduct.id != id;
+                })
+            )
+
+    }
+
+
+    // const getTotalCost = (itemPrices) => {
+    //     let totalCost = 0
+    //     cartProducts.map((cartItem) => {
+    //         totalCost += (itemPrices * cartItem.quantity) 
+    //     })
+    //     return cartProducts
+    // }
+
 
     const contextValue = {
-        items: [],
+        items: cartProducts,
         getProductQuantity,
         addOneToCart,
         removeOneFromCart,
-        deleteFromCart
+        deleteFromCart,
+        // getTotalCost
     }
 
     return (
@@ -26,3 +101,6 @@ export function CartProvider({children}) {
         </CartContext.Provider>
     )
 }
+
+
+export default CartProvider
