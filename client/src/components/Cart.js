4659114079fdd9e2b23edb5b4b6f0335a023677b
genfengl <genfeng.liu@gmail.com>
2022-11-02 14:00:10 +0800

    cart styling

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,44 +1,97 @@
-import Button from "react-bootstrap/esm/Button"
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import { CartContext } from "../CartContext"
 import { useContext } from "react"
+import Catalogue from "./Catalogue"
+import { FaTrash } from "react-icons/fa"
 
 
-const Cart = ({ items, quantity, id } ) => {
+const Cart = ({ items, quantity, id }) => {
     const cart = useContext(CartContext)
 
     //**** Would prefer to use the reduce method here ***
     const cartItems = items?.filter((cartProduct) => {
         return cartProduct._id === id
-       })
-       
-    //    Calculating total price of each product
-      const total = cartItems.reduce((acc, curr) => {
-            return acc + curr.price * quantity
-      }, 0)
-      
+    })
+
+    // //    Calculating total price of each product
+    //   const total = cartItems.reduce((acc, curr) => {
+    //         return acc + curr.price * quantity
+    //   }, 0)
+
 
     return (
-        
-        <div className="cart-dropdown">
+        <div className="py-3">
+
             {cartItems.map((item) => {
                 return (
-                    <>
-                     <h5>{item.title}</h5>
-                     {/* <img src={item.imageURL}/> */}
-                     <p>Qty: {quantity}</p>
-                     <p>${quantity * item.price}</p>
-                     <p>${item.price} each</p>
+                    <Card className=" border-0">
+                        <Row>
+                            <Col xs={3}>
+                                <img src={item.imageURL}
+                                    style={{
+                                        height: '80px',
+                                        width: '80px'
+                                    }}
+                                />
+                            </Col>
+                            <Col xs={9}>
+                                <Row className="mb-3">
+                                    <Col xs={9}>
+                                        {item.title}                                        
+                                    </Col>
+                                    <Col xs={3} className="d-flex justify-content-end">
+                                        x{quantity}
+                                    </Col>
+                                </Row>
+                                <Row className="d-flex align-items-center">
+                                    <Col xs={6} className="text-muted">
+                                        ${item.price} each
+                                    </Col>
+                                    <Col xs={6} className="d-flex justify-content-end lead">
+                                        ${quantity * item.price}
+                                    </Col>
+                                </Row>
+                            </Col>
+
+                        </Row>
+                        <Row className="mt-2 ms-1">
+                            <Col xs={3}>
+                            </Col>
+                            <Col xs={2} className=" text-center fw-bold border-top border-start border-bottom border-1 border-dark">
+                                <Button variant="none" className="fs-5"
+                                    onClick={() => cart.removeOneFromCart(item._id)}>
+                                    -
+                                </Button>
+                            </Col>
+                            <Col xs={2} className=" text-center d-flex align-items-center justify-content-center border-top border-bottom border-1 border-dark">
+                                <div>{quantity}</div>
+                            </Col>
+                            <Col xs={2} className="text-center fw-bold border-top border-end border-bottom border-1 border-dark">
+                                <Button variant="none" className="fs-5"
+                                    onClick={() => cart.addOneToCart(item._id)}>
+                                    +
+                                </Button>
+                            </Col>
+                            
+                            <Col xs={3} className="d-flex justify-content-end">
+                                <Button variant="none" className="p-0"
+                                    onClick={() => cart.deleteFromCart(item._id)}><FaTrash /></Button>
+                            </Col>
 
 
-                     {/* <p>Total: ${total}</p> */}
-                     <Button sm="6" onClick={() => cart.addOneToCart(item._id)} className="mx-2">+</Button>
-                     <Button sm="6" onClick={() => cart.removeOneFromCart(item._id)} className="mx-2">-</Button>
-                     <Button variant="danger" onClick={() => cart.deleteFromCart(item._id)}>Remove Item</Button>
-                     
-                    </>
-                   )
-               })}
-           </div>
+
+                        </Row>
+
+
+                        {/* <p>Total: ${total}</p> */}
+
+                    </Card>
+                )
+            })}
+        </div>
     )
 }
 
--- client/src/components/OffcanvasCart.js
+++ client/src/components/OffcanvasCart.js
@@ -4,40 +4,44 @@ import { CartContext } from "../CartContext"
 import { useContext } from "react"
 import Cart from "./Cart"
 import Button from "react-bootstrap/esm/Button"
+import Row from "react-bootstrap/esm/Row"
 
 
 const OffcanvasCart = ({ showOffcanvasCart, handleOffcanvasCartClose, items }) => {
     const cart = useContext(CartContext)
 
     const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0)
-    
-    //  const cartItems = items?.filter((cartProduct) => {
-    //     return cartProduct._id === id
-    //   })
 
     return (
         <>
             <Offcanvas show={showOffcanvasCart} onHide={handleOffcanvasCartClose} placement='end'>
                 <Container>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>CART</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                <h5>Subtotal ${cart.getTotalCost()}</h5>
-                    { productsCount > 0 ? 
-                        <>
-                            <p>Items in Cart:</p>
-                            {cart.items.map(product => (
-                                <Cart items={items} quantity={product.quantity} id={product.id} />
-                            ))}
-                            <p>Total Items in Cart: {productsCount}</p>
-                           
-                            <Button variant="success">Purchase Items</Button>
-                        </>
-                    :
-                        <p>There are no items in your cart. Buy something, don't be cheap</p>
-                }
-                </Offcanvas.Body>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title className='fs-1 fw-bold text-center py-2'>CART</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <div className="d-flex justify-content-between">
+                            <div className="lead">Subtotal </div>
+                            <div className="lead">${cart.getTotalCost()}</div>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <div className="text-muted">Total Items in Cart </div>
+                            <div className="text-muted">{productsCount}</div>
+                        </div>
+
+                        <div className="border-top border-3">
+                            {productsCount > 0 ?
+                                <>
+                                    {cart.items.map(product => (
+                                        <Cart items={items} quantity={product.quantity} id={product.id} />
+                                    ))}
+                                    <Button variant="success">Purchase Items</Button>
+                                </>
+                                :
+                                <p>There are no items in your cart. Buy something, don't be cheap</p>
+                            }
+                        </div>
+                    </Offcanvas.Body>
                 </Container>
             </Offcanvas>
         </>
