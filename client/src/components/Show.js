4574c6e73efbabc7e03a18155a420430a151d92d
max.nash@lexicondigital.com.au <max.nash@lexicondigital.com.au>
2022-11-02 08:04:40 +0800

    initial favourite button, small profile page updates

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -1,16 +1,19 @@
 import Button from "react-bootstrap/esm/Button"
 import { useParams } from "react-router-dom"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react"
+import { CartContext } from "../CartContext"
+
 
 //need to be styled to have no li styles showing no dotpoint
 
-const handleAddToCart = (event) => {
+const handleFavourite = (event) => {
 
 }
 
 const Show = () => {
     const [item, setItem] = useState(null)
     const { id } = useParams()
+    const cart = useContext(CartContext)
     useEffect(() => {
         const getItem = async () => {
           const res = await fetch(`/api/butcher/${id}`)
@@ -32,7 +35,8 @@ const Show = () => {
                 <li>{item?.description}</li>
                 <li>{item?.category}</li>
                 {item?.stock === 0 ? <li>Sorry, we have no stock remaining</li> : <li>We have {item?.stock} in stock!</li>} </ul> 
-                <Button onClick={handleAddToCart} variant='outline-dark'>Add to Cart</Button>
+                <Button onClick={() => cart.addOneToCart(item._id)} variant='outline-dark'>Add to Cart</Button>
+                <Button onClick={handleFavourite} variant='outline-dark'>Favourite</Button>
         </div>
         </div>
     )
